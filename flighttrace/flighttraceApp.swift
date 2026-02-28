54bd715e44a005acbea753962f8c2d58d311f072
Michael Steinmann <mx1developer@just.fail>
2026-02-28 15:34:11 +1100

    Initial Commit

---
+++ flighttrace/flighttraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  flighttraceApp.swift
+//  flighttrace
+//
+//  Created by Michael Steinmann on 28/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct flighttraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flighttraceTests/flighttraceTests.swift
@@ -0,0 +1,17 @@
+//
+//  flighttraceTests.swift
+//  flighttraceTests
+//
+//  Created by Michael Steinmann on 28/2/2026.
+//
+
+import Testing
+@testable import flighttrace
+
+struct flighttraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
